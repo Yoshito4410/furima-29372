a1ca36c7a80bb3a6cc0f86a252a8cfae8d2c792e
Yoshito4410 <69860007+Yoshito4410@users.noreply.github.com>
2020-09-18 21:09:40 +0900

    add space

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,6 @@ class ItemsController < ApplicationController
   def index
     @items = Items.all
   end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,4 @@ Rails.application.routes.draw do
 
   root to: 'items#index'
 
-
-
-
-
-
 end
